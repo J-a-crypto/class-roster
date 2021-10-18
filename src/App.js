230a9b39f5afea54f48f3c961255d8e41427b78d
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-10-18 17:18:15 -0400

    2021 WCHS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Greeting Fellow Humans</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>01010101010101</h2>
     </div>
   );
 }
