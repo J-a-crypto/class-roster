6bb4132eb32b9d721a3f4a1a70e360d9201ba8f5
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-10-18 17:43:38 -0400

    new change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
     <div className="App">
       <h1>Greeting Fellow Humans</h1>
       <h2>01010101010101</h2>
+      <h3>McDonalds vs Wendys</h3>
+
+      <p>project? nope,</p>
     </div>
   );
 }
