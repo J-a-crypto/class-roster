098e9950e4f84d7aec2c163253c410b253f40892
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-10-25 18:23:55 -0400

    first day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./styles.css";
 
-export default function App() {
+function createListItem(firstname, lastname) {
   return (
-    <div className="App">
-      <h1>Greeting Fellow Humans</h1>
-      <h2>01010101010101</h2>
-      <h3>McDonalds vs Wendys</h3>
-
-      <p>project? nope,</p>
-    </div>
+    <li>
+      {" "}
+      {firstname} {lastname}{" "}
+    </li>
   );
+}
+export default function App() {
+  return <div className="App">{createListItem("Justin", "Apupalo")}</div>;
 }
