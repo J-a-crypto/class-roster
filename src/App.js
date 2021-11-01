8153b8b1e6fccb880b8e6fb2a5c7f8e259e51627
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-11-01 18:29:20 -0400

    Day 2 Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./styles.css";
 
-function createListItem(firstname, lastname) {
-  return (
-    <li>
-      {" "}
-      {firstname} {lastname}{" "}
-    </li>
-  );
-}
+// function createListItem(name,pronouns,school,gradyear,favcolor,favmedia,favpet) {
+//   return (
+//     <li>
+//       <div>{name}</div>|<div>{pronouns}</div>|<div>{school}</div>|<div>{gradyear}</div>|<div>{favcolor}</div>|<div>{favmedia}</div>|<div>{favpet}</div>
+//     </li>
+//   );
+// }
 export default function App() {
-  return <div className="App">{createListItem("Justin", "Apupalo")}</div>;
+  // return <div className="App">
+  // {createListItem("Mikkail Allen","He/His","2021","Blue","TikTok","Dogs")}
+  // {createListItem("Mikkail Allen","He/His","2021","Blue","TikTok","Dogs")}
+  // {createListItem}
+  // In order to use tables you need to import it?
+  // import { useTable } from 'react-table';
+  // this is what I found
+  // </div>;
 }
