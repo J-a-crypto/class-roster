368fe44df91df1d28c36b02e687b05a00d8f6274
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-11-08 19:48:14 -0500

    Day 3 Changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import "./styles.css";
 
-// function createListItem(name,pronouns,school,gradyear,favcolor,favmedia,favpet) {
-//   return (
-//     <li>
-//       <div>{name}</div>|<div>{pronouns}</div>|<div>{school}</div>|<div>{gradyear}</div>|<div>{favcolor}</div>|<div>{favmedia}</div>|<div>{favpet}</div>
-//     </li>
-//   );
-// }
+function displayPerson(first, last, school, year) {
+  return (
+    <div>
+      {" "}
+      {first} {last} {school} {year}{" "}
+    </div>
+  );
+}
+
 export default function App() {
-  // return <div className="App">
-  // {createListItem("Mikkail Allen","He/His","2021","Blue","TikTok","Dogs")}
-  // {createListItem("Mikkail Allen","He/His","2021","Blue","TikTok","Dogs")}
-  // {createListItem}
-  // In order to use tables you need to import it?
-  // import { useTable } from 'react-table';
-  // this is what I found
-  // </div>;
+  let people = [
+    ["Mikkail", "Allen", "WCHS", "2022"],
+    ["Justin", "Apupalo", "NYIT", "2025"],
+    ["Christopher", "Bonifacio", "OCA", "2022"],
+    ["Angel", "Campoverde", "Cristo Rey High School", "2022"],
+    ["Mahdiya", "Chowdhury", "TYWLS", "2023"],
+    ["Diego", "Cruz", "Macalester", "2026"]
+  ];
+
+  let list = [];
+  for (let person of people) {
+    let first = person[0];
+    let last = person[1];
+    let school = person[2];
+    let year = person[3];
+    list.push(displayPerson(first, last, school, year));
+  }
+
+  return <div> {list} </div>;
 }
