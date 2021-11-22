aca8a8a21f6c739d4dce3294c8dc060fda57a199
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-11-22 18:22:50 -0500

    Added the map function into the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-function displayPerson(first, last, school, year) {
+function displayPerson(first, last, school, year, pets) {
   return (
     <div>
       {" "}
@@ -8,29 +8,25 @@ function displayPerson(first, last, school, year) {
     </div>
   );
 }
-
+let people = [
+  ["Mikkail", "Allen", "WCHS", "2022"],
+  ["Justin", "Apupalo", "NYIT", "2025"],
+  ["Christopher", "Bonifacio", "OCA", "2022"],
+  ["Angel", "Campoverde", "Cristo Rey High School", "2022"],
+  ["Mahdiya", "Chowdhury", "TYWLS", "2023"],
+  ["Diego", "Cruz", "Macalester", "2026"]
+];
+let list;
 export default function App() {
-  let people = [
-    ["Mikkail", "Allen", "WCHS", "2022"],
-    ["Justin", "Apupalo", "NYIT", "2025"],
-    ["Christopher", "Bonifacio", "OCA", "2022"],
-    ["Angel", "Campoverde", "Cristo Rey High School", "2022"],
-    ["Mahdiya", "Chowdhury", "TYWLS", "2023"],
-    ["Diego", "Cruz", "Macalester", "2026"]
-  ];
-
-  // let list = [];
-  // for (let person of people) {
-  //   let first = person[0];
-  //   let last = person[1];
-  //   let school = person[2];
-  //   let year = person[3];
-  //   list.push(displayPerson(first, last, school, year));
-  // }
-  let list = people.map(function (person) {
-    return displayPerson(person[0], person[1], person[2], person[3], person[4]);
+  list = people.map(function (person) {
+    // TODO
+    return (
+      <div>
+        {person[0]} {person[1]} {person[2]} {person[3]}
+      </div>
+    );
   });
-  console.log();
-
-  return <div> {list} </div>;
+  return <div className="App">{list}</div>;
 }
+
+///Note: An Array in Array
