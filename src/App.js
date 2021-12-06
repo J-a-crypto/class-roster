f2625565dcb8c3ec64222fb3466c5f136285e0eb
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2021-12-06 18:33:48 -0500

    Making Tables

--- src/App.js
+++ src/App.js
@@ -2,65 +2,77 @@ import "./styles.css";
 
 // import people from "./people.js"
 
-function displayPerson(first, last, school, year, pets) {
-  return (
-    <div>
-      {" "}
-      {first} {last} {school} {year}{" "}
-    </div>
-  );
-}
 let people = [
-  { firstName: "Mikkail", lastName: "Allen", school: "WCHS", gradYear: "2022" },
+  {
+    firstName: "Mikkail",
+    lastName: "Allen",
+    school: "WCHS",
+    gradYear: "2022",
+    color: "blue"
+  },
   {
     firstName: "Justin",
     lastName: "Apupalo",
     school: "NYIT",
-    gradYear: "2025"
+    gradYear: "2025",
+    color: "red"
   },
   {
     firstName: "Christopher",
     lastName: "Bonifacio",
     school: "OCA",
-    gradYear: "2022"
+    gradYear: "2022",
+    color: "red"
   },
   {
     firstName: "Angel",
     lastName: "Campoverde",
     school: "Cristo Rey High School",
-    gradYear: "2022"
+    gradYear: "2022",
+    color: "red"
   },
   {
     firstName: "Mahdiya",
     lastName: "Chowdhury",
     school: "TYWLS",
-    gradYear: "2023"
+    gradYear: "2023",
+    color: "red"
   },
   {
     firstName: "Diego",
     lastName: "Cruz",
     school: "Macalester",
-    gradYear: "2026"
+    gradYear: "2026",
+    color: "red"
   }
 ];
-// let people = [
-//   ["Mikkail", "Allen", "WCHS", "2022"],
-//   ["Justin", "Apupalo", "NYIT", "2025"],
-//   ["Christopher", "Bonifacio", "OCA", "2022"],
-//   ["Angel", "Campoverde", "Cristo Rey High School", "2022"],
-//   ["Mahdiya", "Chowdhury", "TYWLS", "2023"],
-//   ["Diego", "Cruz", "Macalester", "2026"]
-// ];
-let list;
+function setColor() {}
+
 export default function App() {
+  let list;
   list = people.map(function (person) {
     return (
       <div>
-        {person.firstName} {person.lastName} {person.school} {person.gradYear}
+        <thead>
+          <tr>
+            <th style={{ width: "20%" }}></th>
+            <th style={{ width: "20%" }}></th>
+            <th style={{ width: "20%" }}></th>
+            <th style={{ width: "100%" }}></th>
+            <th style={{}}></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{person.firstName}</td>
+            <td>{person.lastName}</td>
+            <td>{person.school}</td>
+            <td>{person.gradYear}</td>
+            <td class={person.color}>{person.color}</td>
+          </tr>
+        </tbody>
       </div>
     );
   });
-  return <div className="App">{list}</div>;
+  return <div className="App"> {list} </div>;
 }
-
-///Note: An Array in Array
