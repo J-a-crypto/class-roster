647ddb42df8b8a87e1af63864624cb87d3534079
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2023-04-21 22:55:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,46 +4,67 @@ import "./styles.css";
 
 let people = [
   {
+    firstName: "First Name ",
+    lastName: "Last Name",
+    school: "School",
+    gradYear: "Year",
+    color: "Color",
+    pronouns: "Pronouns",
+    media: "Media"
+  },
+  {
     firstName: "Mikkail",
     lastName: "Allen",
     school: "WCHS",
     gradYear: "2022",
-    color: "blue"
+    color: "blue",
+    pronouns: "He/Him",
+    media: "Tiktok"
   },
   {
     firstName: "Justin",
     lastName: "Apupalo",
     school: "NYIT",
     gradYear: "2025",
-    color: "red"
+    color: "red",
+    pronouns: "He/Him",
+    media: "Insta"
   },
   {
     firstName: "Christopher",
     lastName: "Bonifacio",
     school: "OCA",
     gradYear: "2022",
-    color: "red"
+    color: "red",
+    pronouns: "He/Him",
+    media: "Insta"
   },
   {
     firstName: "Angel",
     lastName: "Campoverde",
     school: "Cristo Rey High School",
     gradYear: "2022",
-    color: "red"
+    color: "red",
+    pronouns: "He/Him",
+    media: "Insta"
   },
   {
     firstName: "Mahdiya",
     lastName: "Chowdhury",
     school: "TYWLS",
     gradYear: "2023",
-    color: "red"
+    color: "red",
+    pronouns: "She/Her",
+    media: "Tiktok"
   },
   {
     firstName: "Diego",
     lastName: "Cruz",
     school: "Macalester",
     gradYear: "2026",
-    color: "red"
+    color: "red",
+    pronouns: "He/Him",
+    media: "Insta"
   }
 ];
 function setColor() {}
@@ -58,6 +79,7 @@ export default function App() {
             <th style={{ width: "20%" }}></th>
             <th style={{ width: "20%" }}></th>
             <th style={{ width: "20%" }}></th>
+            <th style={{ width: "20%" }}></th>
             <th style={{ width: "100%" }}></th>
             <th style={{}}></th>
           </tr>
@@ -68,7 +90,10 @@ export default function App() {
             <td>{person.lastName}</td>
             <td>{person.school}</td>
             <td>{person.gradYear}</td>
-            <td class={person.color}>{person.color}</td>
+            <td>
+              <span style={{ color: person.color }}>{person.color}</span>{" "}
+            </td>
+            <td>{person.pronouns}</td>
           </tr>
         </tbody>
       </div>
